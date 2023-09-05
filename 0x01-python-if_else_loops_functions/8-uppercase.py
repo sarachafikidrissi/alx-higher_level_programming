8a731669ae4ddb15dafba0d0983c98b65df57ba1
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-05 16:32:40 -0700

    style fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for w in str:
-            char = ord(w)
-            if char >= 97 and char <= 122:
-                char -=32
-            print("{:c}".format(char), end="")
+        char = ord(w)
+        if char >= 97 and char <= 122:
+            char -= 32
+        print("{:c}".format(char), end="")
     print()
