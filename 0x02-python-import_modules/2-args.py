aae58541c2890d6100c95eccd49171f4b9558081
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-07 10:14:15 -0700

    fixing some errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("0 arguments")
+        print("0 arguments.")
 
     else:
         if num_args == 1:
