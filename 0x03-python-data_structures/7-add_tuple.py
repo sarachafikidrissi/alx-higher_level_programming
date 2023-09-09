3e3c3c20fede4f41b58992d47216b303eb32f119
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-09 09:27:52 -0700

    check 4

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) < 2:
         tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
-    
+
     elif len(tuple_b) < 2:
         tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
 
