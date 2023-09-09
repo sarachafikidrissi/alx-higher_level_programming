dc6c108cbb86d827261837fbc810a58141bcf779
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-09 15:16:33 -0700

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    function that returns a tuple with the length
+    of a string and its first character.
+    """
+    info_tuple = len(sentence), sentence[0]
+
+    if len(sentence) == 0:
+        info_tuple[1] = 'None'
+
+    return (info_tuple)
