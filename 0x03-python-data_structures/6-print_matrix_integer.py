35042797d2ac14947c4c9a0698d94d0924ce1e2c
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-09 08:06:20 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+
+    """
+    function that prints a matrix of integers
+    """
     for i in matrix:
-        print²
+        for j in i:
+            print("{:d}".format(j), end=" ")
+        print()
