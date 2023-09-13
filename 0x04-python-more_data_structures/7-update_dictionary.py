4dd93c1351f819e3b68d528db690c1419d319f70
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-13 13:02:50 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    function that replaces or adds key/value in a dictionary.
+    """
+
+    a_dictionary.update({key: value})
+    return (a_dictionary.copy())
