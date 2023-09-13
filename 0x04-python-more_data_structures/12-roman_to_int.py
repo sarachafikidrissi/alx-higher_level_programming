563182e524b4381f943e9b720b868cf1e6bd14f3
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-13 15:53:38 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return (0)
-    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-            'D': 500, 'M': 1000}
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     my_list = list(roman_string.upper())
     result = 0
     prev = 0
@@ -11,7 +10,7 @@ def roman_to_int(roman_string):
         if letter in my_dict:
             result += my_dict[letter]
             if my_dict[letter] > prev:
-                result -= prev *2
+                result -= prev * 2
             prev = my_dict[letter]
         else:
             return (0)
