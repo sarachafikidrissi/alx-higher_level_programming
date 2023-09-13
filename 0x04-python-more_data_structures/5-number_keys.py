4454881a39c3e3850550c2f077063e026e9f9293
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-13 11:04:38 -0700

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    function that returns the number of keys in a dictionary.
+    """
+
+    key_dict = sorted(a_dictionary)
+
+    return (len(key_dict))
