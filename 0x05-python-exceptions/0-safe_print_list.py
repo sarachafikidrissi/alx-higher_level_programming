589c29c4a12b145710320c5e8bb8ec2d0641608f
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-25 09:30:29 -0700

    fixing error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             total_printed += 1
-        except:
+        except IndexError:
             continue
     print()
     return (total_printed)
