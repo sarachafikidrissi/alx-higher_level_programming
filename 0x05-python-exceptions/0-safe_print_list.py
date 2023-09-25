837f8fd3575f8706e005134d3c8550e39f2abfea
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-25 09:25:23 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total_printed = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            total_printed += 1
+        except:
+            continue
+    print()
+    return (total_printed)
