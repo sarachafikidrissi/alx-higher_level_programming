ee94a24f1984a11d5d2790bbec857c87ddd32b4b
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-26 09:16:02 -0700

    adding documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+"""
+created on Tue September 26 2023
+@author: Sara Chafik Idrissi
+"""
+
+
 class Square:
     """Class Square that defines a square.
     """
