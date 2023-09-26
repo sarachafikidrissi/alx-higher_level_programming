06fdd0fcafde2eeead87dd307c586e0250c88971
chafikidrissisara <chafikidrissisara@gmail.com>
2023-09-26 08:58:04 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-
 class Square:
-    """
-    a class representing a square
-
-    Attributes:
-        empty
+    """Class Square that defines a square.
     """
     pass
