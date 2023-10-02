eed4a3fda188b12f68cec14788ae0ed2ec350369
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-02 15:23:06 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,9 +35,9 @@ class Rectangle:
         ValueError: Exception for negative values
     """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
