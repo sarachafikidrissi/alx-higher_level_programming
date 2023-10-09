962384b704ec5a5c8576a10d602c1b602394442d
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-09 15:16:51 -0700

    task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is a 100-my_int module
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from int
+    """
+    def __eq__(self, num):
+        """
+        equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) != num)
+
+    def __ne__(self, num):
+        """
+        no equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) == num)
