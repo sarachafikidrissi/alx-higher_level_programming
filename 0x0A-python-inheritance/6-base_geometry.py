c2bde3821631e3421a806f4d9e57b692f3923954
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-09 13:32:19 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This is 5-base_geometry Class
+"""
+
+
+class BaseGeometry:
+    """
+    Raises:
+        Exception: area() is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
