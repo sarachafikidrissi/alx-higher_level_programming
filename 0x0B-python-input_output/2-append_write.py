9ab60f2ffae587b005f81c775463597a3c04089b
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 11:03:57 -0700

    task 0

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,7 +9,5 @@ def append_write(filename="", text=""):
     """appends a string at the end of a text file (UTF8) and
     returns the number of characters added.
     """
-    with open(filename, 'a', encoding="utf-8") as wf:
-        wf.write(text)
-        readed_char = wf.tell()
-    return (readed_char)
+    with open(filename, 'a', encoding="utf-8") as af:
+        return af.write(text)
