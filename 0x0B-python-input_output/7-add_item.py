8b665e46f62c60b85357834c028b2a1dfd450e90
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 15:06:20 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     new_list = load_from_json_file('add_item.json')
-except:
+except FileNotFoundError:
     new_list = []
 
 for i in range(1, len(sys.argv)):
@@ -18,5 +18,5 @@ for i in range(1, len(sys.argv)):
 
 try:
     save_to_json_file(new_list, 'add_item.json')
-except:
+except IOError:
     pass
