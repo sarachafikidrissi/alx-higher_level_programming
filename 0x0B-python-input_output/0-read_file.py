f3427b5704bfa7b253fdae7928104b5d94c99d9c
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 08:58:30 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a 0-read_file module
+"""
+
+
+def read_file(filename=""):
+    """read a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        f_read = f.read()
+        print(f_read)
