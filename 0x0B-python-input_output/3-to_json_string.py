284c3c5254c4c19eeb4867e1406fa6c382e1ea7e
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 12:46:03 -0700

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,11 +4,15 @@
 
 """This is a 3-to_json_string module
 """
+import json
 
 
 def to_json_string(my_obj):
-    import json
     """encoding a python object to JSON
+    Arguments:
+        my_obj (str): The inputed object to convert into json
+    Return:
+        A json format text
     """
     json_data = json.dumps(my_obj)
     return json_data
