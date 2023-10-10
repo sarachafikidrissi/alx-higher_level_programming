46dd1238de57a0fc9bc7255936db3161d7edadf7
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 15:31:43 -0700

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This is 100-append_after module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line after each line containing a specific string
+
+    Arguments:
+        filename (str): The name of the file
+        search_string (str): The string to math
+        new_string (str): The string to insert after matching
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for i in range(len(lines)):
+        if search_string in lines[i]:
+            lines.insert(i + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        content = "".join(lines)
+        file.write(content)
