828fa29b2df2f10914567fc25cb9f500b5723249
chafikidrissisara <chafikidrissisara@gmail.com>
2023-10-10 15:28:05 -0700

    last task

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This is 12-pascal_triangle module
+"""
+
+
+def pascal_triangle(n):
+    """
+    Creates a pascal triangle
+
+    Attributes:
+        n (int): The n exponent for triangle
+
+    Return:
+        A matrix with values for the triangle
+    """
+    pascal = []
+    triangle = []
+
+    for i in range(int(n)):
+        new = pascal[:]
+        new.append(1)
+        pos = len(pascal)
+        for i in range(1, pos):
+            new[i] = pascal[i - 1] + pascal[i]
+        pascal = new[:]
+        triangle.append(pascal)
+    return triangle
