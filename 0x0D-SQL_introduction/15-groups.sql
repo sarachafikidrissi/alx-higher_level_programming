506034c55c074328412a717645dd280ef2e7f9a4
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-14 16:43:06 -0800

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
---lists the number of records with the same value
+-- Display same score using COUNT, ORDER BY, GROUP BY
+-- DML query to display number of records with same score
 SELECT score,COUNT(*) AS 'number' FROM second_table
 GROUP BY score
 ORDER BY score DESC;
