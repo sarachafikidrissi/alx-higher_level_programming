1ef1393194854b4b51cf8091198753cd9af05f7b
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-15 13:38:36 -0800

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create a dtabase and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT Null,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id));
