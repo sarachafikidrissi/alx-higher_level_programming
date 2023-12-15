a955906e64f52c1badf784b33af798ca1be3bcdd
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-12-15 12:27:58 -0800

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module is for listing states from
+a Database
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+
+    username = args[1]
+    password = args[2]
+    database = args[3]
+    user_input = args[4]
+
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database, port=3306)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE states.name LIKE BINARY '{}'"
+    num_rows = cur.execute(query.format(user_input))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
