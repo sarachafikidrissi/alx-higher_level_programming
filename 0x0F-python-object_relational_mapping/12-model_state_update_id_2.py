4ac98938deee522d0694bfd1d525f915036831b1
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-12-16 11:07:24 -0800

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""start habing conversation with database
+"""
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if (len(args) != 4):
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+    updated_state = session.query(State).filter(State.id == 2).one()
+    updated_state.name = 'New Mexico'
+    session.commit()
