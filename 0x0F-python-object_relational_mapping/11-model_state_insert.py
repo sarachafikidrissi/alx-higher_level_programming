6a7c7566d22b2891b45b18a976e046a75c743ff5
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-12-16 10:57:14 -0800

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""start habing conversation with database
+"""
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if (len(args) != 4):
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
