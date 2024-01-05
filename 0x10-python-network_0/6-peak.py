619896096ae5a759b604e9147fdce32a93916398
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-05 11:07:13 -0800

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this is peak.py module
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Return the peak in a list of integers
+    """
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
