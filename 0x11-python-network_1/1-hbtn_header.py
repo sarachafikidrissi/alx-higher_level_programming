2444b6f7ff50e57d20309f617789601e7b091126
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-06 07:41:45 -0800

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+this is a module to access a specific header response
+"""
+from urllib.request import urlopen
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    with urlopen(url) as response:
+        info = response.info()
+        print(info['X-Request-Id'])
