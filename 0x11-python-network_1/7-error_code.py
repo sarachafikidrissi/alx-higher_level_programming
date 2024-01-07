7a051ef70886977d111e1426541965807e18ef54
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-07 09:00:06 -0800

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+send a request to the url 
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    response = get(url)
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
