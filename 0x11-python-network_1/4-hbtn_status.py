bd721875ba49a1d6ad24fefe7f09c232e1a41ae0
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-07 08:10:03 -0800

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetching a url using requests
+"""
+from requests import get
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    response = get(url)
+    bytes_content = response.text
+    string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
+             type(bytes_content), bytes_content)
+    print(string)
