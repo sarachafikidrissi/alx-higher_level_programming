7eb910a64c548b038f0ba892f297faba1ff4efb0
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-07 08:52:16 -0800

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+send a request to url
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = get(url)
+    info = response.headers
+    print(info.get('x-request-id'))
