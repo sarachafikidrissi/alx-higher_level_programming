2b515dde4e9667e5b8bc80bdf65927e622d39018
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-30 16:07:42 -0800

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filepath = process.argv[2];
+const contentTowrite = process.argv[3];
+
+fs.writeFile(filepath, contentTowrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log();
+});
