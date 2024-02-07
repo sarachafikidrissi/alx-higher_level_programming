6fcce5df177a1e65c861bfc3ddb2e6a8bb6e347f
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-02-07 23:19:57 +0100

    task 12

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('INPUT#btn_translate').click(function () {
+      const lang = $('INPUT#language_code').val();
+  
+      $.ajax({
+        type: 'GET',
+        url: `https://www.fourtonfish.com/hellosalut/?lang=${lang}`,
+        success: function (data) {
+          $('DIV#hello').text(data.hello);
+        }
+      });
+    });
+  });
